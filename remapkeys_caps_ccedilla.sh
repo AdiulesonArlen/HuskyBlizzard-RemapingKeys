b68b8f789982fe1d5b0f82aec0f87229422343c8
Adiuleson Arlen <adiulesonarlen.ads@gmail.com>
2022-10-03 19:46:02 -0300

    Adição de setups
    Adição de outros 2 setups de teclado
    -caps_ccedilla
    -caps_wasd

---
+++ remapkeys_caps_ccedilla.sh
@@ -0,0 +1,10 @@
+xmodmap -e "keycode 66 = Mode_switch" && 
+xmodmap -e "keycode 34 = dead_acute dead_grave Up" && 
+xmodmap -e "keycode 61 = semicolon colon Down" && 
+xmodmap -e "keycode 47 = ccedilla Ccedilla Left" && 
+xmodmap -e "keycode 48 = dead_tilde dead_circumflex Right" &&
+xmodmap -e "keycode 9 = Escape apostrophe quotedbl"
+
+
+
+
---
+++ remapkeys_caps_wasd.sh
@@ -0,0 +1,11 @@
+xmodmap -e "keycode 66 = Mode_switch" && 
+xmodmap -e "keycode 25 = w W Up" && 
+xmodmap -e "keycode 39 = s S Down" && 
+xmodmap -e "keycode 38 = a A Left" && 
+xmodmap -e "keycode 40 = d D Right" &&
+xmodmap -e "keycode 9 = Escape apostrophe quotedbl"
+
+
+
+
+
